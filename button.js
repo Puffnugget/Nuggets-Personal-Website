d3e864132d4233b8381552198a310f81bc2d42de
Puffnugget <95395963+Puffnugget@users.noreply.github.com>
2022-05-27 18:10:32 -0700

    Add files via upload

---
+++ button.js
@@ -0,0 +1,23 @@
+function about(){
+    window.location = "about.html";
+}
+
+function achievements(){
+    window.location = "achievements.html";
+}
+
+function contact(){
+    window.location = "contact.html";
+}
+
+function back(){
+    window.location = "index.html";
+}
+
+function schedule(){
+    window.location = "schedule.html";
+}
+
+function sched(){
+    document.getElementById("schedLbl").innerHTML = "Your meeting has been scheduled!";
+}
